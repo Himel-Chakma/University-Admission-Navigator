6800d0be3a12483c8ca24b2bca746836cf387645
Himel Chakma <u1904131@student.cuet.ac.bd>
2024-12-28 20:34:19 +0600

    Fragment added

--- app/src/main/java/com/edgeproject/universityadmissionnavigator2/HomeActivity.kt
+++ app/src/main/java/com/edgeproject/universityadmissionnavigator2/HomeActivity.kt
@@ -5,16 +5,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.edgeproject.universityadmissionnavigator2.databinding.ActivityHomeBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val navView:BottomNavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_activity)
+        navView.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/edgeproject/universityadmissionnavigator2/UniversityFragment.kt
@@ -0,0 +1,25 @@
+package com.edgeproject.universityadmissionnavigator2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class UniversityFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_university, container, false)
+    }
+}
